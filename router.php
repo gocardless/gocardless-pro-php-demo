a9acb4faec43487f92d74404528ad9f3786d10b4
Iain Nash <iain@gocardless.com>
2015-05-15 13:29:28 +0100

    rm \t

--- router.php
+++ router.php
@@ -21,111 +21,111 @@ $gc = new GoCardlessAPI();
 
 // Define routes
 $router->map('GET', '/', function() use ($twig, $gc) {
-	echo $twig->render('index.html', array('users' => $gc->listUsers(), 'customers' => $gc->listCustomers()));
+    echo $twig->render('index.html', array('users' => $gc->listUsers(), 'customers' => $gc->listCustomers()));
 });
 
 $router->map('POST', '/mandates/create', function() {
-	
+    
 });
 
 $router->map('POST', '/charge/create', function() use ($gc) {
-	if (isset($_POST['mid'])) {
-		$charge = $gc->chargeCustomer($_POST['cid'], $_POST['mid'], 5);
-		header('Location: /customers/' . $_POST['cid'] . '/bill');
-		return;
-	} else if (isset($_POST['cid'])) {
-		$accounts = $gc->client->customer_bank_accounts()->list()->records();
-		$creditors = $gc->client->creditors()->list()->records();
-		if (count($accounts) === 0 || count($creditors) === 0) {
-			throw new Exception('No bank account/creditor found!');
-		}
-		$gc->client->mandates()->create(array(
-		    'links' => array(
-		        'creditor' => $creditors[0]->id(),
-		        'customer_bank_account' => $accounts[0]->id()
-		    )
-		));
-		header('Location: /customers/' . $_POST['cid'] . '/bill');
-		return;
-	}
-	throw new Exception('invalid request!');
+    if (isset($_POST['mid'])) {
+        $charge = $gc->chargeCustomer($_POST['cid'], $_POST['mid'], 5);
+        header('Location: /customers/' . $_POST['cid'] . '/bill');
+        return;
+    } else if (isset($_POST['cid'])) {
+        $accounts = $gc->client->customer_bank_accounts()->list()->records();
+        $creditors = $gc->client->creditors()->list()->records();
+        if (count($accounts) === 0 || count($creditors) === 0) {
+            throw new Exception('No bank account/creditor found!');
+        }
+        $gc->client->mandates()->create(array(
+            'links' => array(
+                'creditor' => $creditors[0]->id(),
+                'customer_bank_account' => $accounts[0]->id()
+            )
+        ));
+        header('Location: /customers/' . $_POST['cid'] . '/bill');
+        return;
+    }
+    throw new Exception('invalid request!');
 });
 
 $router->map('GET', '/customers/[a:cid]/bill', function($cid) use ($twig, $gc) {
-	$customer = $gc->getCustomer($cid);
- 	$mandates = $gc->getCustomerMandates($customer->id())->records();
- 	$mandate = null;
- 	if (count($mandates) > 0) {
- 		$mandate = $mandates[0];
- 	}
- 	$payments = $gc->listPayments($customer->id());
- 	echo $twig->render('bill.html', array(
- 		'payments' => $payments,
- 		'mandates' => $mandates,
- 		'mandate' => $mandate,
- 		'customer' => $customer
- 	));
+    $customer = $gc->getCustomer($cid);
+    $mandates = $gc->getCustomerMandates($customer->id())->records();
+    $mandate = null;
+    if (count($mandates) > 0) {
+        $mandate = $mandates[0];
+    }
+    $payments = $gc->listPayments($customer->id());
+    echo $twig->render('bill.html', array(
+        'payments' => $payments,
+        'mandates' => $mandates,
+        'mandate' => $mandate,
+        'customer' => $customer
+    ));
 });
 
 $router->map('GET', '/oneoff', function() use ($twig) {
-	echo $twig->render('oneoff.html');
+    echo $twig->render('oneoff.html');
 });
 
 $router->map('GET', '/bills/paid_success/[a:fid]', function($fid) use ($gc) {
-	$res = $gc->client->redirect_flows()->complete(array('session_token' => $fid));
-	echo '<pre>';
-	print_r($res);
-	echo '</pre>';
+    $res = $gc->client->redirect_flows()->complete(array('session_token' => $fid));
+    echo '<pre>';
+    print_r($res);
+    echo '</pre>';
 });
 
 $router->map('POST', '/oneoff', function() use ($gc) {
-	if (!isset($_POST['description'])) {
-		throw new Exception('Description required!');
-	}
-	$description = $_POST['description'];
+    if (!isset($_POST['description'])) {
+        throw new Exception('Description required!');
+    }
+    $description = $_POST['description'];
 
-	$creditor_id = $gc->client->creditors()->list()->records()[0]->id();
-	$uid = uniqid();
-	$flow = $gc->client->redirect_flows()->create(array(
-		'session_token' => $uid,
-		'success_redirect_url' => 'http://localhost:8080/bills/paid_success/' . $uid,
-		'description' => $description,
-		'links' => array(
-			'creditor' => $creditor_id
-		)
-	));
-	print_r($flow);
-	header('Location: /flows/' . $flow->id());
+    $creditor_id = $gc->client->creditors()->list()->records()[0]->id();
+    $uid = uniqid();
+    $flow = $gc->client->redirect_flows()->create(array(
+        'session_token' => $uid,
+        'success_redirect_url' => 'http://localhost:8080/bills/paid_success/' . $uid,
+        'description' => $description,
+        'links' => array(
+            'creditor' => $creditor_id
+        )
+    ));
+    print_r($flow);
+    header('Location: /flows/' . $flow->id());
 });
 
 $router->map('GET', '/flows/[a:flow]', function($flow_id) use ($twig, $gc) {
-	$flow = $gc->client->redirect_flows()->get($flow_id);
-	echo $twig->render('flow.html', array('flow' => $flow));
+    $flow = $gc->client->redirect_flows()->get($flow_id);
+    echo $twig->render('flow.html', array('flow' => $flow));
 });
 
 $router->map('GET', '/billed/[a:pid]', function($pid) use ($gc) {
-	$payment = $gc->client->payments()->get($pid);
-	$creditor = $gc->client->creditors()->get($payment->links()->creditor());
-	echo $twig->render('billed.html', array('payment' => $payment, 'creditor' => $creditor));
+    $payment = $gc->client->payments()->get($pid);
+    $creditor = $gc->client->creditors()->get($payment->links()->creditor());
+    echo $twig->render('billed.html', array('payment' => $payment, 'creditor' => $creditor));
 });
 
 $router->map('GET', '/mandate/[a:mid]/pdf', function($mid) use ($gc) {
-	try {
-		$pdfMandate = $gc->client->mandates()->get(
-			$mid,
-			array(),
-			array(
-				'Accept' => 'application/pdf',
-				'Accept-Language' => 'en'
-			)
-		);
+    try {
+        $pdfMandate = $gc->client->mandates()->get(
+            $mid,
+            array(),
+            array(
+                'Accept' => 'application/pdf',
+                'Accept-Language' => 'en'
+            )
+        );
 
-		header('Content-Type: application/pdf');
+        header('Content-Type: application/pdf');
 
-		echo $pdfMandate->response()->raw_body();
-	} catch (\GoCardless\Core\Error\GoCardlessError $e) {
-		echo '<pre>' . print_r($e, true);
-	}
+        echo $pdfMandate->response()->raw_body();
+    } catch (\GoCardless\Core\Error\GoCardlessError $e) {
+        echo '<pre>' . print_r($e, true);
+    }
 });
 
 // match current request url
@@ -133,9 +133,9 @@ $match = $router->match();
 
 // call closure or throw 404 status
 if( $match && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] ); 
+    call_user_func_array( $match['target'], $match['params'] ); 
 } else {
-	// no route was matched
-	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-	echo $twig->render('404.html');
+    // no route was matched
+    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    echo $twig->render('404.html');
 }
